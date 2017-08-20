7571c9a662aab023606c749395a87797a519c078
gemziswt <swtgemzi@gmail.com>
2017-08-20 15:43:30 -0700

    added pg to gemfile

--- db/schema.rb
+++ db/schema.rb
@@ -12,6 +12,9 @@
 
 ActiveRecord::Schema.define(version: 20170819054646) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "items", force: :cascade do |t|
     t.string "title"
     t.text "description"
